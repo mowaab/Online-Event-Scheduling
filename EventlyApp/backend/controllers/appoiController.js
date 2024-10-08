802d079b698fce18ca85782c6caeb4deb0204f16
Sharbel Khoury <sharbelkhoury@Sharbels-MacBook-Pro.local>
2024-10-09 01:03:48 +0200

    deleting and uppdating appointments

--- EventlyApp/backend/controllers/appoiController.js
+++ EventlyApp/backend/controllers/appoiController.js
@@ -1,6 +1,6 @@
 import Appointment from "../models/Appointment.js";
 
-const makeAppointment = async (req,res) => {
+export const makeAppointment = async (req,res) => {
     const{user, title, date, time, description, status} = req.body
     if(!user || !title || !time)
     {
@@ -18,4 +18,37 @@ const makeAppointment = async (req,res) => {
     
 }
 
-export default makeAppointment;
+export const deleteAppointment = async (req, res) => {
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({error:"Appointment does not exist!"})
+    }
+
+    const appointment = await Appointment.findByIdAndDelete({_id: id})
+
+    if(!appointment)
+    {
+        return res.status(400).json({error:"Appointment does not exist!"})
+    }
+
+    res.status(200).json({appointment})
+};
+
+export const updateAppointment = async (req, res) => {
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({error:"Appointment does not exist!"})
+    }
+
+    const appointment = await Appointment.findByIdAndUpdate({_id: id})
+
+    if(!appointment)
+    {
+        return res.status(400).json({error:"Appointment does not exist!"})
+    }
+
+    res.status(200).json({appointment})
+};
+
