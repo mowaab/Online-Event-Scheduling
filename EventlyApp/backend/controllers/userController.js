4eb69546577418b8975e3fb002f73ae3becefef9
Sharbel Khoury <sharbelkhoury@Sharbels-MacBook-Pro.local>
2024-10-09 00:56:07 +0200

    deleting and uppdating users

--- EventlyApp/backend/controllers/userController.js
+++ EventlyApp/backend/controllers/userController.js
@@ -1,6 +1,7 @@
+import mongoose from "mongoose";
 import User from "../models/User.js";
 
-const registerUser = async (req, res) => {
+export const registerUser = async (req, res) => {
     // checking if the user already exists
     const { username, password, email } = req.body;
     if (!username || !password || !email) {
@@ -16,13 +17,48 @@ const registerUser = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
 
         // creating a new user
-        const newUser = User.create({username, email, password: hashedPassword});
+        const user = await User.create({username, email, password: hashedPassword});
         res.status(201).json({ message: 'User registered successfully' });
-        
+
     } catch (error) {
         console.error('Error registering user:', error);
         res.status(500).json({ message: 'Server error' });
     }
 };
 
-export default registerUser;
+export const deleteUser = async (req, res) => {
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({error:"User does not exist!"})
+    }
+
+    const user = await User.findByIdAndDelete({_id: id})
+
+    if(!user)
+    {
+        return res.status(400).json({error:"User does not exist!"})
+    }
+
+    res.status(200).json({user})
+};
+
+export const updateUser = async(req, res) => {
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id))
+    {
+        return res.status(400).json({error:"User does not exist!"})
+    }
+
+    const user = await User.findByIdAndUpdate({_id : id})
+
+    if(!user)
+    {
+        return res.status(400).json({error:"User does not exist!"})
+    }
+
+    res.status(200).json({user})
+
+};
+
